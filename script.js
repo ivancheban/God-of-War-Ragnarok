2b39e6db49974410c300e6d3d6e18656ddc96cea
bcheban <128683111+bcheban@users.noreply.github.com>
2024-08-17 12:35:25 +0300

    added swipers

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
 document.querySelectorAll('include').forEach(function(el) {
     let src = el.getAttribute('src');
     if (src) {
