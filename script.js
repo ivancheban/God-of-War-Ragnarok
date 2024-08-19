82ee6d513d546eb5adeab663fc744d39162141cc
sergey123423213 <grinya.0895@gmail.com>
2024-08-19 13:36:05 +0300

    Update script.js from sergey

--- script.js
+++ script.js
@@ -1,41 +1,63 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-
 document.querySelectorAll('include').forEach(function(el) {
-    let src = el.getAttribute('src');
-    if (src) {
-        fetch(src)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.text();
-            })
-            .then(data => {
-                el.insertAdjacentHTML('beforebegin', data);
-                el.remove();
-            })
-            .catch(error => {
-                console.error('Error fetching the include file:', error);
-            });
-    }
+
+  let src = el.getAttribute('src');
+
+  if (src) {
+
+      fetch(src)
+
+          .then(response => {
+
+              if (!response.ok) {
+
+                  throw new Error('Network response was not ok');
+
+              }
+
+              return response.text();
+
+          })
+
+          .then(data => {
+
+              el.insertAdjacentHTML('beforebegin', data);
+
+              el.remove();
+
+              const swiper = new Swiper('.swiper', {
+
+                  loop: true,
+                slidesPerView:3,
+                  pagination: {
+
+                      el: '.swiper-pagination',
+
+                  },
+
+                  navigation: {
+
+                      nextEl: '.swiper-button-next',
+
+                      prevEl: '.swiper-button-prev',
+
+                  },
+
+                  scrollbar: {
+
+                      el: '.swiper-scrollbar',
+
+                  },
+
+              });
+
+          })
+
+          .catch(error => {
+
+              console.error('Error fetching the include file:', error);
+
+          });
+
+  }
+
 });
