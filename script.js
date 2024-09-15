f7f53d15009b210bbcad20b66d820a0af7ad022c
bcheban <128683111+bcheban@users.noreply.github.com>
2024-09-15 12:21:12 +0300

    added changes

--- script.js
+++ script.js
@@ -234,5 +234,29 @@ function initializeModal() {
       document.getElementById("demo").innerHTML = "EXPIRED";
     }
   }, 1000);
-  // timer - end
+  // timer - 
+  
+  //trailer - start
+  var obj,
+    source;
+
+obj = document.createElement('video');
+$(obj).attr('id', 'example_video_test');
+$(obj).attr('class', 'video-js vjs-default-skin');
+$(obj).attr('width', '640');
+$(obj).attr('data-height', '264');
+$(obj).attr('controls', ' ');
+$(obj).attr('poster', '../images/trailer/trailer-bg.jpg');
+$(obj).attr('preload', 'auto');
+$(obj).attr('data-setup', '{}');
+
+source = document.createElement('source');
+$(source).attr('type', 'video/mp4');
+$(source).attr('src', '../images/trailer/video.mp4');
+
+$("#content").append(obj);
+$(obj).append(source);
+// trailer - end
+}
+  
 }
